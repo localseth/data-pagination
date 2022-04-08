c283ee15a27b33c82c007da191d32f1ba58a96c4
Seth M <97685638+localseth@users.noreply.github.com>
2022-04-08 16:34:52 -0500

    added functionality for: -displaying student information from data file -navigating by groups of 9 -added search bar (does not work yet)

--- js/script.js
+++ js/script.js
@@ -10,20 +10,74 @@ For assistance:
    Reach out in your Slack community if you have questions
 */
 
+//declare variables
+const studentList = document.querySelector('.student-list');
+const linkList = document.querySelector('.link-list');
+const header = document.querySelector('.header');
 
 
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
+//showPage function inserts the HTML in the string literal up to 9 times per page
+//list paramater contains the data to parse, and page number is set to 1 initially then determined by the button the user clicks
+function showPage (list, page) {
+   const indexStart = (page * 9) - 9;
+   const indexEnd = page * 9;
+   studentList.innerHTML = '';
+   for (let i = 0; i < list.length; i++){
+      if (i >= indexStart && i < indexEnd) {
+         let insert =`
+            <li class="student-item cf">
+               <div class="student-details">
+                  <img class="avatar" src ="${list[i].picture.large}" alt="${i.name} Profile Picture">
+                  <h3>${list[i].name.first} ${list[i].name.last}</h3>
+                  <span class="email">${list[i].email}</span>
+               </div>
+               <div class="joined-details">
+                  <span class="date">Joined ${list[i].registered.date}</span
+               </div>
+            </li>
+         `;
+         studentList.insertAdjacentHTML("beforeend", insert);
+      }
+   }
+};
 
+//addPagination creates navigation to access different pages of data
+//once a button is clicked, the showPage function is called with a new value for the page parameter
+function addPagination (list) {
+   const length = Math.ceil(list.length/9);
+   linkList.innerHTML = '';
+   for (let i = 1; i <= length; i++) {
+      let insert =`
+      <li>
+         <button type="button">${i}</button>
+      </li>`;
+      linkList.insertAdjacentHTML("beforeend", insert);
+   }
+   linkList.firstElementChild.firstElementChild.className = 'active';
+   linkList.addEventListener('click', (e) => {
+      const navBtn = e.target;
+      const li = linkList.children;
+      if (navBtn.tagName === 'BUTTON') {
+         for (let i = 0; i < li.length; i++) {
+            li[i].firstElementChild.classList.remove('active');
+         };
+         navBtn.classList.add('active');
+         showPage(list, navBtn.textContent);
+      }
+   });
+};
 
+//search bar
+const searchBar = `
+   <label for="search" class="student-search">
+      <span>Search by name</span>
+      <input id="search" placeholder="Search by name...">
+      <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+   </label>
+`
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
+header.insertAdjacentHTML('beforeend', searchBar);
 
-
-
-// Call functions
+//call both functions to inialize page display
+showPage(data, 1);
+addPagination(data);
