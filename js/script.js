1b02f179150b9ba6a72f1da6d89699d4882064a4
Seth M <97685638+localseth@users.noreply.github.com>
2022-04-11 10:02:12 -0500

    added search bar and search functionality

--- js/script.js
+++ js/script.js
@@ -14,6 +14,18 @@ For assistance:
 const studentList = document.querySelector('.student-list');
 const linkList = document.querySelector('.link-list');
 const header = document.querySelector('.header');
+const searchBar = `
+   <label for="search" class="student-search">
+      <span>Search by name</span>
+      <input id="search" placeholder="Search by name...">
+      <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+   </label>
+`;
+//display search bar on page
+header.insertAdjacentHTML('beforeend', searchBar);
+const search = document.getElementById('search')
+const searchBtn = search.nextElementSibling;
+
 
 
 //showPage function inserts the HTML in the string literal up to 9 times per page
@@ -68,15 +80,32 @@ function addPagination (list) {
 };
 
 //search bar
-const searchBar = `
-   <label for="search" class="student-search">
-      <span>Search by name</span>
-      <input id="search" placeholder="Search by name...">
-      <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
-   </label>
-`
+//add html to display search bar, then declare variables, create function to check for search matches, add listeners to call function
 
-header.insertAdjacentHTML('beforeend', searchBar);
+function searchFn(searchInput, list) {
+   const newArr = list.filter(list =>
+      list.name.first.toLowerCase().includes(searchInput.value.toLowerCase() ) 
+      ||
+      list.name.last.toLowerCase().includes(searchInput.value.toLowerCase() ) 
+   );
+   if (newArr.length > 0){
+      showPage(newArr, 1);
+      addPagination(newArr);
+   } else if (newArr.length === 0){
+      const insert = '<h2>Sorry, no results were found. Please try another search.</h2>';
+      studentList.innerHTML = '';
+      studentList.insertAdjacentHTML("afterbegin", insert);
+   }
+}
+
+searchBtn.addEventListener('click', (e) => {
+   e.preventDefault();
+   searchFn(search, data);
+});
+
+search.addEventListener('keyup', () => {
+   searchFn(search, data);
+});
 
 //call both functions to inialize page display
 showPage(data, 1);
